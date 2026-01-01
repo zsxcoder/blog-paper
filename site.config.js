e5db23ee217566b0666a36e59894e16c86e8b0da
mcyzsx <mcyzsx@outlook.com>
2026-01-01 23:33:34 +0800

    add site configuration file and update gitignore

---
+++ site.config.js
@@ -0,0 +1,25 @@
+export const siteInfo = {
+    author: "钟神秀", // 作者名
+    avatar: "https://home.zsxcoder.top/api/avatar.png",
+    // 头像链接
+  social: {
+    email: "sansuilnm@gmail.com", // 邮件
+    github: "https://github.com/sansui233" // github 链接
+  },
+  // Sites
+  domain: "https://b.zsxcoder.top", // rss 中显示的域名。不影响实际部署
+  walineApi: "https://blog-paper-comment.zsxcoder.top", // 可选项，Waline 评论系统后端地址。不需要请删除。
+  timeZone: "Asia/Shanghai", // 时区配置
+  // friendsApi: "https://checking-link.mcyzsx.top/friends.json", // 可选项，朋友列表 API 地址。如果不配置则使用本地 friends 数组。
+  // 朋友列表（使用本地 ./friends.ts 数据）
+//   friends: [
+//     {
+//       name: "朋友A",
+//       link: "https://example.com"
+//     },
+//     {
+//       name: "朋友B",
+//       link: "https://example.com"
+//     }
+//   ]
+}
